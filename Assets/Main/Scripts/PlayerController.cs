e00b8f8ef14708732689ba0651e45a52784b335a
Abdullah Enes Kocabuğa <75130813+dev-aek@users.noreply.github.com>
2023-12-09 04:49:17 +0300

    hareket

--- Assets/Main/Scripts/PlayerController.cs
+++ Assets/Main/Scripts/PlayerController.cs
@@ -4,14 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public CharacterController controller;
+    CharacterController controller;
     public Transform cam;
 
     public float speed = 6f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+    Rigidbody rgb;
+    public float jumpHeight;
+    public float gravity;
+    public bool isGrounded;
+    Vector3 velocity;
+    public LayerMask layer;
+    public float groudRadius;
+
+    private void Awake()
+    {
+       // rgb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+    }
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
     void Update()
     {
+        isGrounded = Physics.CheckSphere(transform.position, groudRadius, layer);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -1;
+        }
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -25,5 +50,17 @@ public class PlayerController : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+
+        //jumping
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt((jumpHeight * 10) * -2f * gravity);
+        }
+
+        if (velocity.y > -20)
+        {
+            velocity.y += (gravity * 10) * Time.deltaTime;
+        }
+        controller.Move(velocity * Time.deltaTime);
     }
 }
