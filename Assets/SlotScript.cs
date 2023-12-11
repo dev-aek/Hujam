20f2543f973b4807b6ad49fe2f020d84b5c1a82a
Furkan Alliş <furkanallis1905@gmail.com>
2023-12-11 03:35:35 +0300

    sprite added

---
+++ Assets/SlotScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SlotScript : MonoBehaviour, IDropHandler
+{
+    public int id;
+    
+    
+    public void OnDrop(PointerEventData eventData)
+    {
+        
+        if(eventData.pointerDrag != null)
+        {
+            
+            
+
+            if (eventData.pointerDrag.GetComponent<DragAndDrop>().id == id)
+            {
+                eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = this.GetComponent<RectTransform>().anchoredPosition;
+                    
+       
+            }
+                
+            else
+            {
+                eventData.pointerDrag.GetComponent<DragAndDrop>().ResetPosition();
+            }
+            
+        
+            
+            
+        }
+        
+
+        
+    }
+}
